11d6b3f258049b9c3baf2367285a967ebcd59e5c
Giovanni Di Milia <gdimilia@mit.edu>
2016-10-07 15:30:33 -0400

    First test file

---
+++ micromasters/home.py
@@ -0,0 +1,25 @@
+"""
+locust file for /
+"""
+
+from locust import HttpLocust, TaskSet, task
+
+
+class UserBehavior(TaskSet):
+    def on_start(self):
+        """ on_start is called when a Locust start before any task is scheduled """
+        pass
+
+    def login(self):
+        # self.client.post("/login", {"username":"ellen_key", "password":"education"})
+        pass
+
+    @task(1)
+    def index(self):
+        self.client.get("/")
+
+
+class WebsiteUser(HttpLocust):
+    task_set = UserBehavior
+    min_wait = 5000
+    max_wait = 9000
