d49ef08ac92f70236a26531818cf8641dfb584e2
Giovanni Di Milia <gdimilia@mit.edu>
2016-10-17 13:45:25 -0400

    Miltiple tasks

--- micromasters/first_login.py
+++ micromasters/first_login.py
@@ -10,72 +10,64 @@ from locust import HttpLocust, TaskSet, task
 import settings
 
 
-class UserLoginAndProfile(TaskSet):
+class UserDashboardRefresh(TaskSet):
+    """Dashboard refresh"""
+    def on_start(self):
+        """on_start is called before any task is scheduled """
+        self.username = self.parent.username
+        self.mm_csrftoken = self.parent.mm_csrftoken
+        self.profile_filled_out = self.parent.profile_filled_out
 
-    username = None
-    mm_csrftoken = None
+    @task(1)
+    def stop(self):
+        """Return to the parent task"""
+        self.interrupt()
+
+    @task(10)
+    def dashboard_reload(self):
+        """
+        Loads dashboard page and 10 times the backends
+        """
+        if not self.mm_csrftoken or self.profile_filled_out:
+            self.interrupt()
+        # load the page
+        self.client.get('/dashboard/')
+        self.client.get(
+            '/api/v0/profiles/{}/'.format(self.username),
+            name="'/api/v0/profiles/[username]/"
+        )
+        self.client.get('/api/v0/dashboard/')
+        self.client.get('/api/v0/course_prices/')
+        self.client.get('/api/v0/enrolledprograms/')
+        # reload 10 times the dashboard api to simulate refresh waiting for enrollment
+        # enrollment not tested
+        for _ in range(10):
+            self.client.get('/api/v0/dashboard/')
+
+
+class UserTab1(TaskSet):
+    """First Tab of the user profile"""
+    tasks = [UserDashboardRefresh]
+    profile_filled_out = False
 
     def on_start(self):
-        """ on_start is called when a Locust start before any task is scheduled """
-        self.username = random.choice(settings.USERNAMES_IN_EDX)
+        """on_start is called before any task is scheduled """
+        self.username = self.parent.username
+        self.mm_csrftoken = self.parent.mm_csrftoken
 
-    def login(self):
-        """
-        Function to login an user on MicroMasters assuming she has an account on edX
-        """
-        # load the login form to get the token
-        login_form = self.client.get(
-            urljoin(settings.EDXORG_BASE_URL, '/login'),
-            name='/login[edx login page]',
-        )
-        cookies = login_form.cookies.get_dict()
-        # login edx
-        self.client.post(
-            urljoin(settings.EDXORG_BASE_URL, '/user_api/v1/account/login_session/'),
-            data={
-                "email": "{}@example.com".format(self.username),
-                "password": "test",
-                'remember': 'false'
-            },
-            headers={'Referer': urljoin(settings.EDXORG_BASE_URL, '/login'),
-                     'X-CSRFToken': cookies.get('csrftoken')},
-            name='/user_api/v1/account/login_session/[edx login form]'
-        )
-        # login micromasters
-        self.client.get(
-            '/login/edxorg/',
-            name='/login/edxorg/[micromasters]'
-        )
-        # get the csrftoken
-        parsed_url = urlparse(settings.MICROMASTERS_BASE_URL)
-        domain = parsed_url.netloc
-        if ':' in domain:
-            domain = domain.split(':')[0]
-        self.mm_csrftoken = self.client.cookies.get('csrftoken', domain=domain)
+    @task(1)
+    def stop(self):
+        """Return to the parent task"""
+        self.interrupt()
 
-    def logout(self):
-        """
-        Logout from edx and micromasters
-        """
-        # logout edX
-        self.client.get(
-            urljoin(settings.EDXORG_BASE_URL, '/logout'),
-            name='/logout[edx]'
-        )
-        # logout micromasters
-        self.client.get(
-            '/logout',
-            name='/logout[micromasters]'
-        )
-
-    def index_no_login(self):
-        """Load index page without being logged in"""
-        self.client.get("/")
-
+    @task(2)
     def profile_tabs(self):
         """
         Profile tabs
         """
+        if self.mm_csrftoken is None:
+            self.interrupt()
+
         # loading part
         self.client.get('/profile/')
         resp_profile = self.client.get(
@@ -211,42 +203,96 @@ class UserLoginAndProfile(TaskSet):
                 name="'/api/v0/profiles/[username]/",
             )
 
-    def dashboard_reload(self):
-        """
-        Loads dashboard page and 10 times the backends
-        """
-        # load the page
-        self.client.get('/dashboard/')
-        self.client.get(
-            '/api/v0/profiles/{}/'.format(self.username),
-            name="'/api/v0/profiles/[username]/"
-        )
-        self.client.get('/api/v0/dashboard/')
-        self.client.get('/api/v0/course_prices/')
-        self.client.get('/api/v0/enrolledprograms/')
-        # reload 10 times the dashboard api to simulate refresh waiting for enrollment
-        # enrollment not tested
-        for _ in range(10):
-            self.client.get('/api/v0/dashboard/')
+        self.profile_filled_out = True
+
+
+class UserLogIn(TaskSet):
+    """
+    Section for login
+    """
+    tasks = {UserTab1: 3}
+
+    def on_start(self):
+        """on_start is called before any task is scheduled """
+        self.username = self.parent.username
+        self.mm_csrftoken = self.parent.mm_csrftoken
 
     @task
-    def login_and_profile(self):
+    def stop(self):
+        """Return to the parent task"""
+        self.interrupt()
+
+    @task
+    def login(self):
         """
-        The actual task with the different operations in the right sequence to be run by locust
+        Function to login an user on MicroMasters assuming she has an account on edX
         """
-        self.index_no_login()
+        # load the login form to get the token
+        login_form = self.client.get(
+            urljoin(settings.EDXORG_BASE_URL, '/login'),
+            name='/login[edx login page]',
+        )
+        cookies = login_form.cookies.get_dict()
+        # login edx
+        self.client.post(
+            urljoin(settings.EDXORG_BASE_URL, '/user_api/v1/account/login_session/'),
+            data={
+                "email": "{}@example.com".format(self.username),
+                "password": "test",
+                'remember': 'false'
+            },
+            headers={'Referer': urljoin(settings.EDXORG_BASE_URL, '/login'),
+                     'X-CSRFToken': cookies.get('csrftoken')},
+            name='/user_api/v1/account/login_session/[edx login form]'
+        )
+        # login micromasters
+        self.client.get(
+            '/login/edxorg/',
+            name='/login/edxorg/[micromasters]'
+        )
+        # get the csrftoken
+        parsed_url = urlparse(settings.MICROMASTERS_BASE_URL)
+        domain = parsed_url.netloc
+        if ':' in domain:
+            domain = domain.split(':')[0]
+        self.mm_csrftoken = self.client.cookies.get('csrftoken', domain=domain)
 
-        self.login()
+    @task
+    def logout(self):
+        """
+        Logout from edx and micromasters
+        """
+        # logout edX
+        self.client.get(
+            urljoin(settings.EDXORG_BASE_URL, '/logout'),
+            name='/logout[edx]'
+        )
+        # logout micromasters
+        self.client.get(
+            '/logout',
+            name='/logout[micromasters]'
+        )
+        self.mm_csrftoken = None
 
-        self.profile_tabs()
 
-        self.dashboard_reload()
+class UserBehavior(TaskSet):
 
-        self.logout()
+    tasks = [UserLogIn]
+    username = None
+    mm_csrftoken = None
+
+    def on_start(self):
+        """on_start is called before any task is scheduled """
+        self.username = random.choice(settings.USERNAMES_IN_EDX)
+
+    @task
+    def index_no_login(self):
+        """Load index page without being logged in"""
+        self.client.get("/")
 
 
 class WebsiteUser(HttpLocust):
     host = settings.MICROMASTERS_BASE_URL
-    task_set = UserLoginAndProfile
+    task_set = UserBehavior
     min_wait = settings.LOCUST_TASK_MIN_WAIT
     max_wait = settings.LOCUST_TASK_MAX_WAIT
